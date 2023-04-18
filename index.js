da3588a49aef8ed80a34470ee8c38485d5e49887
Nayeemmnt999 <nayeem.mnt999@gmail.com>
2023-04-18 08:02:21 +0600

    fixed data

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express();
+const phone = require('./phone.json')
 const port = 5000;
 
 app.get('/', (req, res) => {
     res.send('hi this is nayeem rahman farhad')
 })
+app.get('/phone', (req, res) => {
+    res.send(phone)
+})
+app.get('/phone/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log(id);
+    const singlePhone = phone.find(pi => pi.id === id || {} );
+    res.send(singlePhone)
+})
+
 app.listen(port, () => {
     console.log(`this port running in ${port}`);
 })
